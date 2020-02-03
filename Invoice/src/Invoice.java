fc4e436f58670f3858f50dc9a4718a9a17b4bfad
Godzillafan74 <55116540+Godzillafan74@users.noreply.github.com>
2020-02-02 20:15:22 -0500

    Add files via upload

---
+++ Invoice/src/Invoice.java
@@ -0,0 +1,37 @@
+
+public class Invoice {
+	private int hours;
+	private double rate; 
+	private double expenses;
+	private String name;
+	private double hourlycharges;
+	
+	public Invoice(String inName, String inPart) {
+		name = inName;
+		String part = inPart;
+	}
+	
+	public void settimeworked(int inhours) {
+		hours = inhours;
+	}
+	
+	public void setexpenses(double inrate) {
+		expenses = inrate;
+	}
+	
+	public void setrate(double inrate) {
+		rate = inrate;
+	}
+	
+	public void computelaborcharges() { 
+		hourlycharges = rate*hours;
+	}
+	
+	public double computetotalcharges() {
+		return hourlycharges + expenses;
+	}
+	
+	public void printinvoice() {
+		System.out.println("Name of Project: "+ name + "Total number of hours worked: " + hours + "Expenses: "+ expenses + "Rate charged: "+ rate + "Total amount charged: "+ (hourlycharges+expenses));
+	}
+}
---
+++ Invoice/src/InvoiceTester.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InvoiceTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		Invoice newinvoice = new Invoice();
+		
+		
+	}
+
+}
